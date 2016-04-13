73953f8e8c38c5226b2ee5e4b22b043627216800
Quinn Hosler <qhos93@gmail.com>
2016-04-13 23:11:30 +0000

    changes to install

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@
 # check if PHP is installed
 php -v >> /dev/null
 if [ $? -ne 0 ]; then echo "PHP is not installed"; exit 1; fi
+echo "php is installed correctly"
 
 apache2 -v >> /dev/null
 if [ $? -ne 0 ]; then echo "PHP is not installed"; exit 1; fi
+echo "apache is installed correctly"
 
-# May break, depends on current version of Composer
-php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
-php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+if [ ! -e "composer.phar" ]; then
+	# May break, depends on current version of Composer
+	php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+	php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	php composer-setup.php
+	php -r "unlink('composer-setup.php');"
+fi
 
-if [ ! -a "composer.phar" ]; then echo "Composer install failed"; exit 1; fi
+if [ ! -e "composer.phar" ]; then echo "Composer install failed"; exit 1; fi
 
 # change permissions for composer (just in case)
 sudo chmod +x composer.phar
